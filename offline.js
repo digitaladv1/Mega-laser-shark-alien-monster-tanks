1b2a65f376eeca2a8434b7dbdb4ff2f22b1fd4f5
Jim <digital1@digitaleducationadventures.com>
2018-01-05 16:44:44 -0600

    Initial commit

---
+++ offline.js
@@ -0,0 +1,1 @@
+{"version":1515191199493,"fileList":["pathfind.js","c2runtime.js","data.js","start.js","style.css","offlineClient.js","images/shared-0-sheet0.png","icons/loading-logo.png","icons/icon-32.png","icons/icon-64.png","icons/icon-256.png","icons/icon-16.png","icons/icon-512.png","icons/icon-128.png","register-sw.js"]}
